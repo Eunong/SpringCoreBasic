a28064e735ea65d80718953f2859374e340524a5
Eunong <hoodyong@gmail.com>
2023-02-10 02:14:44 +0900

    섹션 8. 빈 생명주기 콜백 - 빈 생명주기 이해 - 생명주기 콜백 사용방법 실습     (1) InitializingBean, DisposableBean 인터페이스 구현     (2) 설정 정보에 빈 등록 초기화/종료 메서드 지정     (3) @PostConstruct, @PreDestroy 애노테이션 활용 => 이 방법을 사용하면 됨!

---
+++ src/test/java/org/spring/lifecycle/NetworkClient.java
@@ -0,0 +1,71 @@
+package org.spring.lifecycle;
+
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class NetworkClient {
+// public class NetworkClient implements InitializingBean, DisposableBean {
+
+    private String url;
+
+    public NetworkClient() {
+        System.out.println("생성자 호출, url = " + url);
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    // 서비스 시작 시 호출
+    public void connect() {
+        System.out.println("connect: " + url);
+    }
+
+    public void call(String message) {
+        System.out.println("call: " + url + " message = " + message);
+    }
+
+    // 서비스 종료 시 호출
+    public void disconnect() {
+        System.out.println("close: " + url);
+    }
+
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.init");
+        connect();
+        call("초기화 연결 메시지");
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.close");
+        disconnect();
+    }
+
+    /*
+    *//**
+     * 의존관계 주입이 끝나면 호출
+     * @throws Exception
+     *//*
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    *//**
+     * 스프링 빈 소멸 전 호출
+     * @throws Exception
+     *//*
+    @Override
+    public void destroy() throws Exception {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
+    }*/
+
+}
